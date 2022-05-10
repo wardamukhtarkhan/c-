b0c45e93388016922abf8056ec408ed3ba772d17
wardamukhtarkhan <102410054+wardamukhtarkhan@users.noreply.github.com>
2022-05-10 18:32:29 +0500

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num=5;
+    
+    if (num==5)
+    {
+        cout<<"ok";
+    }
+    else
+    {
+    cout << "not ok";
+    }
+   
+    return 0;
+}
